596a2b3fcbbc16005200069856622a9a26a8faf8
Willianamoba <willian@amobasoftware.com>
2025-04-27 15:04:34 -0400

    first commit

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,19 +1,10 @@
-import { RegisterOptions, UseFormRegister } from "react-hook-form";
-
-import { FieldValues } from "react-hook-form";
-import { Path } from "react-hook-form";
-
-type InputTypeProps<T extends FieldValues> = {
+type InputTypeProps = {
     label: string
     placeholder?: string
     type?: string
-    register?: UseFormRegister<T>
-    registerField?: Path<T>
-    validations?: RegisterOptions<T, Path<T>>
-    errors?: string
 }
 
-export function Input<T extends FieldValues>({label, placeholder, type='text', register, registerField, validations, errors}: InputTypeProps<T>) {
+export function Input({label, placeholder, type='text'}: InputTypeProps) {
 
     return (
         <div className="flex flex-col space-y-2">
@@ -22,14 +13,8 @@ export function Input<T extends FieldValues>({label, placeholder, type='text', r
                 type={type} 
                 id={label} 
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:outline-none focus:ring focus:ring-orange-500" 
-                placeholder={placeholder} 
-                {...register && registerField && {
-                    ...register(registerField, validations)
-                }}    
+                placeholder={placeholder}   
             />
-            <span className="text-red-500 text-sm">
-                {errors}
-            </span>
 
         </div>
     );
--- src/RegisterForm.tsx
+++ src/RegisterForm.tsx
@@ -3,19 +3,15 @@ import { Input } from "./Input";
 
 type RegisterForm = {
     name: string;
-    email: string;
-    password: string;
 }
 
 export function RegisterForm() {
 
     const initialValues = {
         name: '',
-        email: '',
-        password: ''
     }
 
-    const {register, handleSubmit, formState: {errors}} = useForm({defaultValues: initialValues})
+    const {handleSubmit} = useForm({defaultValues: initialValues})
 
     const onSubmit = (data: RegisterForm) => {
         console.log(data);
@@ -28,21 +24,6 @@ export function RegisterForm() {
                 <Input
                     label='Nombre'
                     placeholder='Ingrese su nombre'
-                    register={register}
-                    registerField="name"
-                    validations={
-                        {
-                            required: {
-                                value: true,
-                                message: 'Nombre es requerido'
-                            },
-                            minLength: {
-                                value: 3,
-                                message: 'Nombre debe tener al menos 3 caracteres'
-                            }
-                        }
-                    }
-                    errors={errors.name?.message}
                 />
                 
                 <button type="submit" className="w-full bg-orange-600 text-white py-2 rounded hover:bg-orange-700 transition duration-200">Register</button>
